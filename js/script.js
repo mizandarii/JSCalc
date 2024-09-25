3943b4d7ba843d7ee44dce09d7fb2b1423f4caf1
Edvin <edvinture@mail.ru>
2024-09-25 12:48:52 +0300

    updated index.html, added curren.json and script.js

---
+++ js/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const inputEur = document.querySelector('#eur'),
+    inputUsd = document.querySelector('#usd');
+    inputRub = document.querySelector('#rub');
+
+    inputEur.addEventListener('input', () =>{
+        const request = new XMLHttpRequest();
+
+        request.open('GET', 'js/current.json');
+        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+        request.send();
+
+        request.addEventListener('load', ()=>{
+            if(request.status === 200){
+                const data = JSON.parse(request.response);
+
+                inputUsd.value = (+inputEur.value * data.current.usd).toFixed(2);
+                inputRub.value = (+inputEur.value * data.current.rub).toFixed(2);
+            }else{
+                inputUsd.value = "Something went wrong";
+                inputRub.value = "Something went wrong";
+            }
+        });
+    });
